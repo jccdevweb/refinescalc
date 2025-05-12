6a59cd0ad0553768a388ecb10f86a35625005161
Joseph Charles Vargas <jcc.devweb@gmail.com>
2025-05-12 12:23:40 +0800

    some updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Refines from "@/app/components/Refines";
 import RefinesTax from "@/app/components/RefinesDynamic";
 export default function Home() {
   return (
     <div className="">
-      {/* <Refines /> */}
       <RefinesTax />
     </div>
   );
